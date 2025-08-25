0ffbb53d6f75735684a6a1aaf8a3e758985f5859
Nakibul Huda <nakibulhuda780@gmail.com>
2025-08-25 18:32:41 +0600

    Remove

--- ShareMotion/root/server/src/routes/polling.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPollingData } = require('../controllers/polling.controller');
-const auth = require('../middleware/auth.middleware');
-
-router.get('/data', auth, getPollingData);
-
-module.exports = router;
